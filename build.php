0bd37fa4aeb2fdd6275a86489e3f74c3a641f9a0
Matthew Baggett <matthew@baggett.me>
2019-08-13 18:30:56 +0200

    Add docker login step

--- build.php
+++ build.php
@@ -29,6 +29,7 @@ $travisYaml = [
         ]
     ],
     'before_script' => [
+        'docker login -u $DOCKER_LOGIN -p $DOCKER_PASSWORD $DOCKER_REGISTRY',
         'sudo rm /usr/local/bin/docker-compose',
         'curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` > docker-compose',
         'chmod +x docker-compose',
